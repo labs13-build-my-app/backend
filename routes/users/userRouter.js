2493c3bb885974ec9486289f5e415640a3ac266b
Arkadiusz Kusiak <akusiak23@gmail.com>
2019-05-30 18:13:32 -0500

    userRouter

---
+++ routes/users/userRouter.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const data = require("./userModel");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await data.findUsers();
+    if (users.length) {
+      res.status(200).json({
+        error: false,
+        message: "The users were found in the database",
+        users
+      });
+    } else {
+      res.status(404).json({
+        error: true,
+        users: [],
+        message: "The users could not be found in the database."
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: `User request failed ${error.message}.`
+    });
+  }
+});
