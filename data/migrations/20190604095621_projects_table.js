36de8952f875c318f956344bae90891b58e931e2
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-06-04 14:32:45 -0700

    added plans and project tables added new field for user tbl, run seeds

---
+++ data/migrations/20190604095621_projects_table.js
@@ -0,0 +1,25 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("projects", tbl => {
+    tbl.increments();
+    tbl.string("name", 255).notNullable();
+    tbl.string("description", 255).notNullable();
+    tbl.string("image_url", 255);
+    tbl.integer("budget").notNullable();
+    tbl.text("dueDate").notNullable();
+    tbl.string("projectStatus", 30).notNullable();
+    tbl.string("paymentStatus", 30).notNullable();
+    tbl.string("feedback", 600).notNullable();
+    tbl
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("projects");
+};
