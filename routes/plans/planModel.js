97f2db9955da6021c1f07c52674b0c4392ab31fc
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-06-05 18:39:11 -0400

    basic endpoints listed for plan API

---
+++ routes/plans/planModel.js
@@ -0,0 +1,13 @@
+const db = require("../../data/dbConfig.js");
+
+const getPlanByID = async id => {};
+const addPlan = async plan => {};
+const updatePlan = async plan => {};
+const deletePlan = async id => {};
+
+module.exports = {
+  getPlanByID,
+  addPlan,
+  updatePlan,
+  deletePlan
+};
