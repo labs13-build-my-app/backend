5b776c7b48bd4a6cc2606c2da55502b25e9a46c4
joe-alfaro <josephlalfaro@gmail.com>
2019-06-06 08:44:48 -0700

    Complete route to see projects based on project owner, not dynamic without login

--- routes/projects/index.js
+++ routes/projects/index.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const Projects = require('./model');
+
 const testingProjectsRouter = (req, res) => {
     console.log("here in projects, looks like it works");
     res.send("testing projects route, looks like it works");
@@ -11,17 +16,31 @@ const testingProjectsRouter = (req, res) => {
     );
   },
   listProjectOwnersProposals = (req, res) => {
-    res.send("endpoint to retrieve list of project owner proposals");
+    const projectOwner_id = req.user_id;
+
+    //Projects.findByProjectOwner(projectOwner_id)
+    //Delete this line and line below and uncomment line above when login is finished
+    Projects.findByProjectOwner(51)
+      .then(projects => {
+        res
+          .status(200)
+          .json(projects);
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json(error);
+      });
   },
   projectOwnersProposal = (req, res) => {
-    res.send("endpoint to retrieve a project owners proposal");
+    res.send("endpoint to retrieve project owner's proposal");
   };
 
 module.exports = router => {
   router.get("/test-projects", testingProjectsRouter);
   router.get("/plan-list", listDevelopersPlans);
   router.get("/submitted-plan/:plan-id", developersPlan);
-  router.get("/proposal-list", listProjectOwnersProposals);
+  router.get("/project-owner", listProjectOwnersProposals);
   router.get("/proposal/:proposal-id", projectOwnersProposal);
 
   return router;
