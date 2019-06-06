f9e1eaa0c69b2b1f8ed94849f907f726341f2844
Arkadiusz Kusiak <akusiak23@gmail.com>
2019-06-06 12:38:15 -0500

    Implement GET for all projects

--- routes/projects/index.js
+++ routes/projects/index.js
@@ -6,6 +6,22 @@ const testingProjectsRouter = (req, res) => {
     console.log("here in projects, looks like it works");
     res.send("testing projects route, looks like it works");
   },
+  getAllProjects = async (req, res) => {
+    try {
+      const projects = await data.getAllProjects();
+      if (projects) {
+        res.status(200).json(projects);
+      } else {
+        res.status(404).json({
+          message: "The projects could not be found in the database."
+        });
+      }
+    } catch (error) {
+      res.status(500).josn({
+        message: `Project request failed ${error.message}.`
+      });
+    }
+  },
   listDevelopersPlans = (req, res) => {
     res.send("endpoint to retrieve list of developer plans");
   },
@@ -38,6 +54,7 @@ const testingProjectsRouter = (req, res) => {
 
 module.exports = router => {
   router.get("/test-projects", testingProjectsRouter);
+  router.get("/list-all-projects", getAllProjects);
   router.get("/plan-list", listDevelopersPlans);
   router.get("/submitted-plan/:id", developersPlan);
   router.get("/project-list", listProjectOwnersProjects);
