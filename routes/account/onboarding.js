59be7f00ea0ff793c9810ef7115702be06b4f13f
nomadic4life <orbison.dennis@gmail.com>
2019-06-08 23:27:22 -0700

    fix bug, userSignup route is now working like it should

--- routes/account/onboarding.js
+++ routes/account/onboarding.js
@@ -18,52 +18,53 @@ const testOnboardingRoute = (req, res) => {
       twitter
     } = req.body;
 
-    if (role !== "Developer" || role !== "Project Owner") {
-      res
-        .status(500)
-        .json({ message: "invalid user type, please submit valid user type" });
-    }
-
-    createNewUser({
-      sub,
-      role,
-      firstName,
-      lastName,
-      email,
-      skills,
-      devType,
-      linkedIn,
-      gitHub,
-      twitter
-    })
-      .then(userid => {
-        const id = userid[0];
-        res.status(201).json({
-          id,
-          role,
-          firstName,
-          lastName,
-          email,
-          skills,
-          devType,
-          linkedIn,
-          gitHub,
-          twitter
-        });
+    if (role === "Developer" || role === "Project Owner") {
+      createNewUser({
+        sub,
+        role,
+        firstName,
+        lastName,
+        email,
+        skills,
+        devType,
+        linkedIn,
+        gitHub,
+        twitter
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({
+        .then(userid => {
+          const id = userid[0];
+          res.status(201).json({
+            id,
+            role,
+            firstName,
+            lastName,
+            email,
+            skills,
+            devType,
+            linkedIn,
+            gitHub,
+            twitter
+          });
+        })
+        .catch(err => {
+          res.status(500).json({
             message: "unable to create user, please try again",
             error: err
           });
-      });
+        });
+    } else {
+      res
+        .status(200)
+        .json({ message: "invalid user type, please submit valid user type" });
+    }
   },
   userLogin = async (req, res) => {
     const { user_id: sub } = req;
     findAuthorizedUser(sub)
       .then(user => {
+        if (!user) {
+          res.status(200).json({ message: "please signup" });
+        }
         res.status(200).json(user);
       })
       .catch(err => {
