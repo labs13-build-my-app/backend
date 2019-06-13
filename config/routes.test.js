c0c829d9306bf551f7cb572e863e22e679a4f204
nomadic4life <orbison.dennis@gmail.com>
2019-06-13 02:44:54 -0700

    added some random testing to test out this jest test thing. will be writing some real tests now

--- config/routes.test.js
+++ config/routes.test.js
@@ -13,4 +13,24 @@ describe("GET /", () => {
       .get("/")
       .expect(200);
   });
+
+  it("testing endpoin in users", () => {
+    return request(server)
+      .get("/api/users/test-users")
+      .expect("testing users route, looks like it works");
+  });
+
+  it("get all users", () => {
+    return request(server)
+      .get("/api/users/list-all-users")
+      .expect(200)
+      .expect("Content-Type", /json/);
+  });
+
+  it("get all developers", () => {
+    return request(server)
+      .get("/api/users/developers")
+      .expect(200)
+      .expect("Content-Type", /json/);
+  });
 });
